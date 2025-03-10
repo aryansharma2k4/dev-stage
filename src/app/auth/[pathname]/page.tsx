382417caa2328dcc5df9e626b0d2e1cea584e4af
Aryan Sharma <aryansharma2k4@gmail.com>
2025-03-11 00:28:08 +0530

    Checking the validity of the husky tests second time

--- src/app/auth/[pathname]/page.tsx
+++ src/app/auth/[pathname]/page.tsx
@@ -1,19 +1,15 @@
 import { AuthCard } from "@daveyplate/better-auth-ui"
 import { authViewPaths } from "@daveyplate/better-auth-ui/server"
-import "@/styles/globals.css";
- 
+import "@/styles/globals.css"
+
 export function generateStaticParams() {
     return Object.values(authViewPaths).map((pathname) => ({ pathname }))
 }
- 
-export default async function AuthPage(
-    { params }: { params: Promise<{ pathname: string }> }
-) {
-    const { pathname } = await params
- 
+
+export default function AuthPage() {
     return (
         <div className="flex flex-col items-center my-auto">
-            <AuthCard pathname={pathname} />
+            <AuthCard pathname="sign-in" />
         </div>
     )
 }
